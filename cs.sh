9939648367454ea7dce77eea40d05354240bce96
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-05 01:24:48 +0700

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://172.200.229.146:80
+POOL=stratum+ssl://20.114.150.102:80
 WALLET=ZEPHsBL4wFydCvcmNJEHc7iR7Fq6NcZtFHsSAV7GpztYDzZtyanCTShcMd7d2n96yMHhioHisWLpJWGcvaAstUYm2itMLaQKzhk.CPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )
 myip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
