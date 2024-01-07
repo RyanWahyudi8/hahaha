b10edf162950b3b12f9f8ece310887ce755e0ea8
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-08 05:29:39 +0700

    Update bziron.sh

--- bziron.sh
+++ bziron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-POOL=stratum+ssl://54.235.18.60:443
+POOL=stratum+ssl://54.236.252.140:443
 ALGO=ironfish
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71+20491097150.GPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
