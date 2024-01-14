18dd73b9d5d9b1510cbfef8e811e9605b5ed37db
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-14 23:43:45 +0700

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=stratum+ssl://54.236.252.140:80
+POOL=stratum+ssl://104.45.192.96:80
 WALLET=ZEPHsBL4wFydCvcmNJEHc7iR7Fq6NcZtFHsSAV7GpztYDzZtyanCTShcMd7d2n96yMHhioHisWLpJWGcvaAstUYm2itMLaQKzhk.CPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )
 myip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
-chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-url=54.236.252.140:2012 --cc-access-token=kevin --cc-worker-id=${myip} --cuda
+chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-url=104.45.192.96:2012 --cc-access-token=kevin --cc-worker-id=${myip} --cuda
