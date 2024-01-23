a8322307974ae9407315836c2c7405d6caa766de
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-23 23:41:40 +0700

    Update bziron.sh

--- bziron.sh
+++ bziron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 POOL=stratum+ssl://3.68.199.229:443
 ALGO=ironfish
-WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71+20491097150.GPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )
+WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71+20491097150.GPU_$(echo "$(curl icanhazip.com)" | tr . _ )
 cd "$(dirname "$0")"
 chmod +x ./bzminer && clear && ./bzminer -a $ALGO -w $WALLET -p $POOL --nc 1  $@
