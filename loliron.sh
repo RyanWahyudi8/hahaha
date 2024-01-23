08d900040961f621af9045f23ab2702045f266b4
Ryan Wahyudi <116867564+RyanWahyudi8@users.noreply.github.com>
2024-01-23 23:36:36 +0700

    Update loliron.sh

--- loliron.sh
+++ loliron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=stratum+ssl://3.85.165.149:443
+POOL=stratum+ssl://3.68.199.229:443
 ALGO=ALEPH
 WALLET=IRON:757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71:20491097150.GPU_$(echo "$(curl -s ifconfig.me)" | tr . _ )#lus6-e35u
 cd "$(dirname "$0")"
